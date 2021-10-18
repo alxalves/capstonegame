78748441e79c2efcf9457264f1d3005f7726a8bf
alxalves <alxalves2@gmail.com>
2021-10-17 20:55:29 -0400

    MenuScene, main game menu

---
+++ client/src/scenes/MenuScene.js
@@ -0,0 +1,45 @@
+import BaseScene from "./BaseScene";
+
+
+class MenuScene extends BaseScene {
+  constructor(config) {
+    super("MenuScene", config);
+
+    this.homePage = [
+        {scene: 'PlayScene', text: "Play"},
+        {scene: 'ScoreScene', text: "Score"},
+        {scene: null, text: 'Exit'},
+    ]
+  }
+
+  create() {
+    super.create();   
+    this.createHomePage(this.homePage, this.createHomePageEvent.bind(this)); 
+  }
+
+  createHomePageEvent(homePageItem) {
+      const textGO = homePageItem.textGO;
+      console.log(homePageItem);
+      textGO.setInteractive();
+
+      textGO.on('pointerover', () => {
+          textGO.setStyle({fill: '#e7deff'})
+      })
+
+      textGO.on("pointerout", () => {
+        textGO.setStyle({ fill: "#fff" });
+      });
+      textGO.on('pointerup', () => {
+          console.log(homePageItem.scene)
+          homePageItem.scene && this.scene.start(homePageItem.scene);
+
+        if(homePageItem.text === 'Exit') {
+            this.game.destroy(true);
+        }
+
+      })
+  }
+}
+
+
+export default MenuScene;
