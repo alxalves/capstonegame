e73d43a14ac0f2fde96bd29c043f0ccd0c286cc1
alxalves <alxalves2@gmail.com>
2021-10-17 20:36:51 -0400

    test highscore data

--- server/controller/controller.js
+++ server/controller/controller.js
@@ -6,7 +6,54 @@ const mongoURI = "mongodb://localhost:27017/" + "capstonegame";
 const db = mongoose.connection;
 
 router.get("/", (req, res) => {
-    res.send("I'm both respecting your privacy by knocking but asserting my authority as your father by coming in anyway!")
+    res.send("... Dinkleberg...")
+})
+
+router.get("/highscores", (req, res) => {
+    let hs = [
+      {
+        username: "God",
+        score: 696969,
+      },
+      {
+        username: "Satan",
+        score: 666666,
+      },
+      {
+        username: "Alex",
+        score: 55555,
+      },
+      {
+        username: "Yo Mamma",
+        score: 55554,
+      },
+      {
+        username: "Daddy",
+        score: 6620,
+      },
+      {
+          username: "Test",
+          score: 6601
+      },
+      {
+          username: "Test2",
+          score: 6601
+      },
+      {
+          username: "Test3",
+          score: 5999
+      },
+      {
+          username: "Test4",
+          score: 4888
+      },
+      {
+          username: "Test5",
+          score: 4333
+      },
+    ];
+
+    res.json(hs);
 })
 
 
